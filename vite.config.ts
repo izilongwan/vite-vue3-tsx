134d9db7c41509366516411942e6dedd7a0989d3
izilongwan <js.nb@qq.com>
2023-03-21 21:24:24 +0800

    feat: add api proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,51 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import styleImport from "vite-plugin-style-import";
+import { resolve } from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import styleImport from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
-export default ({ mode }) =>
-  defineConfig({
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
     plugins: [
       vue(),
       vueJsx(),
       styleImport({
         libs: [
           {
-            libraryName: "element-plus",
+            libraryName: 'element-plus',
             esModule: true,
             ensureStyleFile: true,
             resolveStyle: (name) => {
-              return `element-plus/lib/theme-chalk/${name}.css`;
+              return `element-plus/lib/theme-chalk/${ name }.css`
             },
             resolveComponent: (name) => {
-              return `element-plus/lib/${name}`;
+              return `element-plus/lib/${ name }`
             },
           },
         ],
       }),
     ],
-    // base: mode === "development" ? "/" : "./",
+    // base: mode === 'development' ? '/' : './',
     server: {
       port: 8888,
+      proxy: {
+        [env.VITE_API_URL]: {
+          target: env.VITE_API_PROXY_URL,
+          changeOrigin: true,
+          rewrite: path => path.replace(new RegExp(`^${ env.VITE_API_URL }`), ''),
+        },
+      }
     },
     resolve: {
       alias: {
-        "@": resolve(__dirname, "/src"),
+        '@': resolve(__dirname, '/src'),
       },
     },
     build: {
       sourcemap: true,
     }
-  });
+  })
+}
