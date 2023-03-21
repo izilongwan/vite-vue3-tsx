bac90b10121e0d167f907232305cdc26f3ad2e5d
izilongwan <js.nb@qq.com>
2023-03-21 21:32:16 +0800

    fix: 修正API路径

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,9 @@
 import { http } from '@/util/http'
 
+const { VITE_API_URL } = import.meta.env
+
 export function getTestManu() {
   return http({
-    url: '/api/test/manu2'
+    url: `${ VITE_API_URL }/test/manu`
   })
 }
--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_APP_TITLES: string[]
+  readonly VITE_API_URL: string
   // 更多环境变量...
 }
 
