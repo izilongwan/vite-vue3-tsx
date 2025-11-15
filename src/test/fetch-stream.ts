8da606c0dbfece2194f0c224d05bb38acc7f826a
izilongwan <js.nb@qq.com>
2025-11-15 20:05:56 +0800

    feat: ✨ 移动downloadByBlob函数至工具模块以提高代码组织性

--- src/test/fetch-stream.ts
+++ src/test/fetch-stream.ts
@@ -33,12 +33,3 @@ export function fetchStream(filename: string) {
     }
     )
 }
-
-function downloadByBlob(blob: Blob, fileName: string) {
-  const url = URL.createObjectURL(blob);
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = fileName;
-  a.click();
-  URL.revokeObjectURL(url);
-}
--- src/util/tool.ts
+++ src/util/tool.ts
@@ -15,3 +15,12 @@ export function getProxyUrlMap(env: Record<string, string>) {
     return p
   }, {})
 }
+
+export function downloadByBlob(blob: Blob, fileName: string) {
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = fileName;
+  a.click();
+  URL.revokeObjectURL(url);
+}
