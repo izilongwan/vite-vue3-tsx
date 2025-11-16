fa946b513d0956abead502b08278c45fa0080934
izilongwan <js.nb@qq.com>
2025-11-16 15:42:45 +0800

    feat: ✨ 重构fetchStream为FetchStream类，支持动态URL和进度回调

--- src/util/fetch-stream.ts
+++ src/util/fetch-stream.ts
@@ -1,38 +1,46 @@
-export function fetchStream(filename: string) {
-  fetch(`http://127.0.0.1:9000/file/stream/${ filename }`)
-    .then(async rs => {
-      const total = Number(rs.headers.get('Content-Length'));
-      const reader = rs.body?.getReader();
-      let size = 0;
-      const chunks: BlobPart[] = [];
-
-      while (true) {
-        const { done, value } = await reader!.read();
-        if (done) {
-          const blob = new Blob(chunks);
-          const url = URL.createObjectURL(blob);
-          console.log(url);
-          break;
-        }
-        size += value.length;
-        console.log((size / total * 100).toFixed(2) + '%');
-        chunks.push(value);
-      }
-
-      // reader?.read()
-      //   .then(async function _({ done, value }) {
-      //     if (done) {
-      //       const blob = new Blob(chunks);
-      //       const url = URL.createObjectURL(blob);
-      //       console.log(url);
-      //       return;
-      //     }
-      //     console.log(size += value.length, total, size / total);
-      //     chunks.push(value);
-      //     reader?.read().then(_);
-      //   })
-    }
-    )
+export interface FetchStreamOptions {
+  url: string;
+  onProcess?(percentage: number): void;
+  onFinish?(blob: Blob): void;
 }
 
-// fetchStream('1.mp4');
+export class FetchStream {
+  #options: FetchStreamOptions;
+
+  constructor(option: FetchStreamOptions) {
+    this.#options = option;
+    this.#doFetch();
+  }
+
+  #doFetch() {
+    fetch(this.#options.url)
+      .then(async rs => {
+        const total = Number(rs.headers.get('Content-Length'));
+        const reader = rs.body?.getReader();
+        let size = 0;
+        const chunks: BlobPart[] = [];
+
+        while (true) {
+          const { done, value } = await reader!.read();
+          if (done) {
+            const blob = new Blob(chunks);
+            this.#onFinish(blob);
+            break;
+          }
+          size += value.length;
+          const percentage = (size / total * 100).toFixed(2);
+          this.#onProcess?.(Number(percentage));
+          chunks.push(value);
+        }
+      }
+      );
+  }
+
+  #onProcess(percentage: number): void {
+    this.#options.onProcess?.(percentage);
+  }
+
+  #onFinish(blob: Blob): void {
+    this.#options.onFinish?.(blob);
+  }
+}
