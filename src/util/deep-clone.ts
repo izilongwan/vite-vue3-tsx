5c6b7fc2650e9903da431d16bea4885a0e59ce30
izilongwan <js.nb@qq.com>
2025-11-25 18:41:54 +0800

    fix: 🐛 更新deepClone函数以支持Set和Map类型的深拷贝

--- src/util/deep-clone.ts
+++ src/util/deep-clone.ts
@@ -1,32 +1,47 @@
-import { TypeCommonObject } from '@/d.types/common';
+import { TypeCommonConstructor, TypeCommonObject } from '@/d.types/common';
 
 export function deepClone<T extends TypeCommonObject>(target: T, source: T = <T> {}): T {
   const weakMap = new WeakMap();
   const CONSTRUCTORS_LIST = [Date, RegExp];
+  const TYPE_REG = /^\[object (.+)\]$/
 
   const cloneObj = (function _(target, weakMap) {
     if (!target || typeof target !== 'object') {
       return target;
     }
 
-    const constructor = target.constructor;
-    const regDateConstructor = <RegExpConstructor | DateConstructor> constructor;
+    const constructor = <TypeCommonConstructor> target.constructor;
 
-    if (CONSTRUCTORS_LIST.includes(regDateConstructor)) {
-      return new regDateConstructor(<any> target);
+    if (CONSTRUCTORS_LIST.includes(constructor)) {
+      return new constructor(target);
     }
 
     if (weakMap.has(target)) {
       return weakMap.get(target);
     }
 
-    const cloneObj: TypeCommonObject = new (<ArrayConstructor | ObjectConstructor> constructor)();
+    const cloneObj: TypeCommonObject = new constructor();
 
     weakMap.set(target, cloneObj);
 
     for (const key in target) {
       if (Object.hasOwn(target, key)) {
-        cloneObj[key] = _(target[key], weakMap);
+        const originValue = target[key];
+        const typeString = Object.prototype.toString.call(originValue);
+        const value = _(originValue, weakMap);
+        const [__, type] = typeString.match(TYPE_REG) || [];
+
+        switch (type) {
+          case 'Set':
+            (<Set<unknown>> cloneObj).add(value);
+            break;
+          case 'Map':
+            (<Map<unknown, unknown>> cloneObj).set(key, value);
+            break;
+          default:
+            cloneObj[key] = value;
+            break;
+        }
       }
     }
 
